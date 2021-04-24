24b3335c35da23bd025347078bdc32197977d67a
thomas darmawan <thomas.darmawan9@gmail.com>
2021-04-24 12:06:11 +0700

    asd

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost:3307',
+	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => '',
-	'database' => 'ims',
+	'database' => 'manainven',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
